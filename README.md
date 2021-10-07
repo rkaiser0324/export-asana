ea337ab00be8dc8cb3edc0666b2d10692a30da03
Rolf Kaiser <1090569+rkaiser0324@users.noreply.github.com>
2021-10-07 09:18:15 -0700

    feat: switch to HTML format, including attachment links and subtasks

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 # export-asana
 
-This is a PHP CLI script which uses the Asana REST API to export, to a CSV, all the Tasks and Comments in a given Project in a Workspace.
+This is a PHP CLI script, inspired by [this forum post](https://forum.asana.com/t/exporting-projects-including-comments/53796/2), which uses the Asana REST API to export to HTML, all the Tasks and Comments in a given Project in a Workspace.
+
+
 
 ## Setup
 
 1. `composer install`
+1. Get a Personal Access Token per [these instructions](https://developers.asana.com/docs/authentication-quick-start).
 1.  Create `./config.php` as
     ```php
     <?php
-    // Set this to a Personal Access Token found in Asana Account Settings
+    // Set this to your Personal Access Token
     define('ASANA_ACCESS_TOKEN', 'xxx');
     ```
 
 ## Usage
 
 ```
-php export.php <workspacename> <projectname> <outputcsvfilename>
+php export.php <workspacename> <projectname> <outputfilename>
 ```
 
 E.g., 
 ```
-php export.php "My Workspace" "My Project" output.csv
+php export.php "My Workspace" "My Project" output.html
 ```
--- export.php
+++ export.php
@@ -2,16 +2,14 @@
 /**
  * Use the Asana API to export all tasks and comments for a given project.
  *
- * export.php <workspacename> <projectname> <outputcsvfilename>
+ * export.php <workspacename> <projectname> <outputfilename>
  *
  * E.g.,
- * export.php "My Workspace" "My Project" output.csv
+ * export.php "My Workspace" "My Project" output.html
  *
  */
 
 require dirname(__FILE__) . '/vendor/autoload.php';
-
-require dirname(__FILE__) . '/config.php';
 
 use Asana\Client;
 
@@ -36,10 +34,16 @@ if (!$fp = fopen($filename, 'w')) {
     throw new exception("Cannot open $filename for writing");
 }
 
-echo "Getting tasks for project $project_name in workspace $workspace_name...\n";
+require dirname(__FILE__) . '/config.php';
+
+if (!defined('ASANA_ACCESS_TOKEN')) {
+    throw new exception("Please define ASANA_ACCESS_TOKEN in ./config.php");
+}
 
 // create a $client->with a Personal Access Token
-$client = Asana\Client::accessToken(ASANA_ACCESS_TOKEN);
+$client = Asana\Client::accessToken(ASANA_ACCESS_TOKEN, array('headers' => array('asana-disable' => 'new_user_task_lists')));
+
+echo "Getting tasks for project \"$project_name\" in workspace \"$workspace_name\"...\n";
 
 $me = $client->users->me();
 
@@ -53,19 +57,22 @@ foreach ($me->workspaces as $w) {
         foreach ($projects as $p) {
             if ($p->name == $project_name) {
                 $project_id = $p->gid;
+
+                $i = 0;
                 foreach ($client->tasks->findAll(array('project' => $project_id), array('page_size' => 100)) as $t) {
-                    $comments = [];
-                    foreach ($client->stories->getStoriesForTask($t->gid, array(), array('opt_pretty' => 'true')) as $story) {
-                        if ($story->type == 'comment') {
-                            //var_dump($story);
-                            $comments[$story->created_at] = $story->text;
-                        }
+                    // if ($t->gid != '459300356162719') {
+                    //     continue;
+                    // }
+
+                    $result = get_task($client, $t->gid);
+
+                    if ($result['status'] == 'OK') {
+                        $tasks[] = $result['task'];
                     }
-                    $tasks[$t->gid] = [
-                        'name' => $t->name,
-                        'comments' => $comments
-                    ];
-                    printf("\t%s\n", $t->name);
+
+                    printf("%s - %s - %s\n", ++$i, $result['status'], $result['task']['name']);
+
+                    //if ($i>10) break;
                 }
             }
         }
@@ -73,18 +80,152 @@ foreach ($me->workspaces as $w) {
     }
 }
 
-fputcsv($fp, ["Task ID", "Name", "URL", "Comment Date", "Comment"]);
+// Sort tasks by created_at
+usort($tasks, function ($a, $b) {
+    return ($a['created_at'] < $b['created_at']) ? -1 : 1;
+});
 
-foreach ($tasks as $task_id => $task) {
-    $task_url = sprintf('https://app.asana.com/0/%s/%s', $project_id, $task_id);
+fprintf($fp, '<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    fputcsv($fp, [$task_id, $task['name'], $task_url]);
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    foreach ($task['comments'] as $comment_timestamp => $comment_text) {
-        fputcsv($fp, ['', '', '', $comment_timestamp, $comment_text]);
+    <title>Asana Tasks - %s</title>
+    <style type="text/css">
+    dt, dl {
+        font-size: 0.875em;
     }
+    </style>
+  </head>
+    <body>
+        <div class="container">
+        <h1>Asana Tasks - %s</h1>
+        ', $project_name, $project_name);
+
+foreach ($tasks as $task) {
+    print_task($fp, $project_id, $task);
 }
 
+fprintf($fp, "
+        </div><!-- end container -->
+    </body>
+</html>");
 fclose($fp);
 
-echo "Output written to $filename.\n";
+echo "\nOutput written to $filename.\n";
+
+function format_timestamp($str)
+{
+    return strftime('%m/%d/%y %r %Z', strtotime($str));
+}
+
+function get_task($client, $gid)
+{
+    $status = "OK";
+    $comments = [];
+    $subtasks = [];
+
+    $task_data = $client->tasks->getTask($gid);
+  
+    // Skip any task that is in multiple projects
+    if (count($task_data->projects) > 1) {
+        $status = "SKIP";
+    } else {
+        foreach ($client->stories->getStoriesForTask($gid, array(), array('opt_pretty' => 'true')) as $story) {
+            if ($story->type == 'comment') {
+                $comments[] = [
+                    'created_at' => $story->created_at,
+                    'text' => $story->text
+                ];
+            }
+        }
+
+        foreach ($client->attachments->getAttachmentsForTask($gid, array(), array('opt_pretty' => 'true')) as $attachment) {
+            $attachment_data = $client->attachments->findById($attachment->gid);
+            $comments[] = [
+                'created_at' => $attachment_data->created_at,
+                'text' => $attachment_data->name,
+                'url' => $attachment_data->view_url,
+            ];
+        }
+
+        usort($comments, function ($a, $b) {
+            return ($a['created_at'] < $b['created_at']) ? -1 : 1;
+        });
+
+        foreach ($client->tasks->getSubtasksForTask($gid, array(), array('opt_pretty' => 'true')) as $subtask) {
+            $result = get_task($client, $subtask->gid);
+            if ($result['status'] == 'OK') {
+                $subtasks[] = $result['task'];
+            }
+        }
+
+        usort($subtasks, function ($a, $b) {
+            return ($a['created_at'] < $b['created_at']) ? -1 : 1;
+        });
+    }
+    
+    return [
+            'status' => $status,
+            'task' => [
+                'gid' => $task_data->gid,
+                'assignee' => $task_data->assignee->name ?? '',
+                'created_at' => $task_data->created_at,
+                'completed_at' => $task_data->completed ? format_timestamp($task_data->completed_at) : '',
+                'name' => $task_data->name,
+                'notes' => $task_data->notes,
+                'comments' => $comments,
+                'subtasks' => $subtasks
+            ]
+        ];
+}
+
+function print_task($fp, $project_id, $task)
+{
+    $task_url = sprintf('https://app.asana.com/0/%s/%s', $project_id, $task['gid']);
+
+    fprintf($fp, '
+    <div class="row">
+        <div class="col-12">
+            <p><strong>%s</strong></p>
+            <p class="small">%s</p>
+            <dl class="row">
+                <dt class="col-sm-2">URL</dt>
+                <dd class="col-sm-10"><a href="%s" target="_new">%s</a></dd>
+                <dt class="col-sm-2">Created At</dt>
+                <dd class="col-sm-10">%s</dd>
+                <dt class="col-sm-2">Completed At</dt>
+                <dd class="col-sm-10">%s</dd>
+            </dl>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-11">
+', htmlspecialchars($task['name']), nl2br(htmlspecialchars($task['notes'])), $task_url, $task_url, format_timestamp($task['created_at']), $task['completed_at']);
+
+    foreach ($task['comments'] as $comment) {
+        $text = nl2br(htmlspecialchars($comment['text']));
+        if (!empty($comment['url'])) {
+            $text = sprintf('<a href="%s" target="_new">%s</a>', $comment['url'], $comment['text']);
+        }
+        fprintf($fp, '
+            <dl class="row">
+                <dt class="col-2">%s</dt>
+                <dd class="col-10">%s</dd>
+            </dl>
+        ', format_timestamp($comment['created_at']), $text);
+    }
+
+    foreach ($task['subtasks'] as $subtask) {
+        print_task($fp, $project_id, $subtask);
+    }
+
+    fprintf($fp, "
+        </div>
+    </div><!-- end task -->
+    ");
+}
